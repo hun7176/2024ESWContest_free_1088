e4a20393b845510b895cab692b8d6c0f949b4dc5
hun7176 <hun7176@naver.com>
2024-07-27 17:20:55 +0900

    uart permmition

--- bird_turret/src/rasptostm.py
+++ bird_turret/src/rasptostm.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-import os
 import rospy
 from geometry_msgs.msg import Point
 import serial
-import time
 
 class UART_START:
     def __init__(self):
         rospy.init_node('rasptostm', anonymous=True)
         self.coordinate_sub = rospy.Subscriber('/burd_turret/coordinates', Point, self.callback)
+        self.ser = serial.Serial("/dev/ttyAMA2", baudrate=115200, timeout=1)
+        rospy.on_shutdown(self.cleanup)
 
     def callback(self, data):
         try:
-            ser = serial.Serial("/dev/ttyAMA2", baudrate=115200, timeout=1)
-            while True:
-                ser.write(f'X:{data.x},Y:{data.y},Z:{data.z}\n'.encode())
-                print(f'Sent coordinates: X={data.x}, Y={data.y}, Z={data.z}')
-                time.sleep(1)
-        except KeyboardInterrupt:
-            print('Interrupted')
-            ser.close()
+            self.ser.write(f'X:{data.x},Y:{data.y},Z:{data.z}\n'.encode())
+            rospy.loginfo(f'Sent coordinates: X={data.x}, Y={data.y}, Z={data.z}')
+        except Exception as e:
+            rospy.logerr(f'Error writing to serial port: {e}')
+
+    def cleanup(self):
+        self.ser.close()
+        rospy.loginfo("Serial port closed.")
 
     def run(self):
         rospy.spin()
@@ -27,3 +27,4 @@ class UART_START:
 if __name__ == '__main__':
     my_uart = UART_START()
     my_uart.run()
+
