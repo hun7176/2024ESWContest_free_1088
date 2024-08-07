11fcb8b70e203a794a9ec6c1f0acb5d50a93c4a8
curie <hyle0710@naver.com>
2024-08-07 15:29:27 +0900

    수정

--- bird_camera/image_compress/src/image_compressor.cpp
+++ bird_camera/image_compress/src/image_compressor.cpp
@@ -7,18 +7,35 @@
 class ImageCompressor
 {
 public:
-    ImageCompressor(const std::string& input_topic, const std::string& output_topic)
+    ImageCompressor()
         : it_(nh_)
     {
-        image_sub_ = it_.subscribe(input_topic, 1, &ImageCompressor::imageCallback, this);
-        image_pub_ = it_.advertise(output_topic, 1);
+        // 첫 번째 이미지 구독자 및 퍼블리셔 설정
+        image_sub1_ = it_.subscribe("/usb_cam1/image_raw", 1, &ImageCompressor::imageCallback1, this);
+        image_pub1_ = it_.advertise("/usb_cam1/image_compressed", 1);
+
+        // 두 번째 이미지 구독자 및 퍼블리셔 설정
+        image_sub2_ = it_.subscribe("/usb_cam2/image_raw", 1, &ImageCompressor::imageCallback2, this);
+        image_pub2_ = it_.advertise("/usb_cam2/image_compressed", 1);
     }
 
-    void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+    void imageCallback1(const sensor_msgs::ImageConstPtr& msg)
+    {
+        processImage(msg, image_pub1_);
+    }
+
+    void imageCallback2(const sensor_msgs::ImageConstPtr& msg)
+    {
+        processImage(msg, image_pub2_);
+    }
+
+private:
+    void processImage(const sensor_msgs::ImageConstPtr& msg, const image_transport::Publisher& pub)
     {
         cv_bridge::CvImagePtr cv_ptr;
         try
         {
+            // ROS 이미지 메시지를 OpenCV 이미지로 변환
             cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
         }
         catch (cv_bridge::Exception& e)
@@ -27,45 +44,39 @@ public:
             return;
         }
 
-        // 해상도 낮추기
+        // 이미지 해상도 낮추기
         cv::Mat resized_image;
         cv::resize(cv_ptr->image, resized_image, cv::Size(), 0.5, 0.5);
 
-        // 압축
+        // 이미지 압축
         std::vector<uchar> buf;
         cv::imencode(".jpg", resized_image, buf);
 
-        // 이미지 메시지로 변환
+        // 압축된 이미지 데이터를 OpenCV 형식으로 디코딩
+        cv::Mat decoded_image = cv::imdecode(buf, cv::IMREAD_COLOR);
+
+        // OpenCV 이미지를 ROS 이미지 메시지로 변환
         cv_bridge::CvImage out_msg;
         out_msg.header = msg->header;
         out_msg.encoding = sensor_msgs::image_encodings::BGR8;
-        out_msg.image = cv::imdecode(buf, cv::IMREAD_COLOR);
+        out_msg.image = decoded_image;
 
         // 퍼블리시
-        image_pub_.publish(out_msg.toImageMsg());
+        pub.publish(out_msg.toImageMsg());
     }
 
-private:
     ros::NodeHandle nh_;
     image_transport::ImageTransport it_;
-    image_transport::Subscriber image_sub_;
-    image_transport::Publisher image_pub_;
+    image_transport::Subscriber image_sub1_;
+    image_transport::Subscriber image_sub2_;
+    image_transport::Publisher image_pub1_;
+    image_transport::Publisher image_pub2_;
 };
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "image_compressor");
-
-    if (argc != 3) {
-        ROS_ERROR("Usage: image_compressor <input_topic> <output_topic>");
-        return -1;
-    }
-
-    std::string input_topic = argv[1];
-    std::string output_topic = argv[2];
-
-    ImageCompressor ic(input_topic, output_topic);
+    ImageCompressor ic;
     ros::spin();
     return 0;
 }
-
--- bird_camera/image_compressor/src/image_compressor.py
+++ bird_camera/image_compressor/src/image_compressor.py
@@ -5,16 +5,14 @@ import cv2
 
 class ImageCompressor:
     def __init__(self):
-        rospy.init_node('image_compressor', anonymous=True)  
+        rospy.init_node('image_compressor', anonymous=True)
         self.bridge = CvBridge()
         
-        # Subscribers for both image topics
         self.image_sub1 = rospy.Subscriber('/usb_cam1/image_raw', Image, self.image_callback1)
         self.image_sub2 = rospy.Subscriber('/usb_cam2/image_raw', Image, self.image_callback2)
         
-        # Publishers for compressed images
-        self.image_pub1 = rospy.Publisher('/usb_cam1/image_raw/compressed', CompressedImage, queue_size=10)
-        self.image_pub2 = rospy.Publisher('/usb_cam2/image_raw/compressed', CompressedImage, queue_size=10)
+        self.image_pub1 = rospy.Publisher('/usb_cam1/image_compressed', CompressedImage, queue_size=10)
+        self.image_pub2 = rospy.Publisher('/usb_cam2/image_compressed', CompressedImage, queue_size=10)
 
     def image_callback1(self, data):
         self.compress_and_publish(data, self.image_pub1)
@@ -24,19 +22,14 @@ class ImageCompressor:
 
     def compress_and_publish(self, data, publisher):
         try:
-            # Convert ROS image message to OpenCV image
             cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-            
-            # Compress OpenCV image
             _, compressed_image = cv2.imencode('.jpg', cv_image, [cv2.IMWRITE_JPEG_QUALITY, 90])
             
-            # Convert compressed image to ROS CompressedImage message
             compressed_msg = CompressedImage()
             compressed_msg.header = data.header
             compressed_msg.format = "jpeg"
             compressed_msg.data = compressed_image.tobytes()
 
-            # Publish compressed image message
             publisher.publish(compressed_msg)
         except Exception as e:
             rospy.logerr(f"Error processing image: {e}")
