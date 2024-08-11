4e7a172785d980e137cf52a77b0b14fb3f6179a4
hun7176 <hun7176@naver.com>
2024-08-11 16:38:20 +0900

    core해결1

--- bird_core/src/core.py
+++ bird_core/src/core.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 import rospy
+import roslaunch
 from std_msgs.msg import Int32
 from geometry_msgs.msg import Twist
 
@@ -26,10 +28,47 @@ class AutonomousVehicleNode:
         # Twist message for controlling the vehicle
         self.twist = Twist()
 
+        # Base path
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+
+        # Relative Launch Management
+        self.detection_1_launch_file = os.path.join(base_path, 'bird_camera/bird_detection_1/launch/bird_detection_1.launch')
+        self.detection_2_launch_file = os.path.join(base_path, 'bird_turret/bird_detection_2/launch/bird_detection_2.launch')
+
+        self.detection_1_launch = None
+        self.detection_2_launch = None
+
+        # Start detection 1
+        self.start_detection_1()
+
+    def start_detection_1(self):
+        rospy.loginfo("Starting detection 1")
+        self.detection_1_launch = roslaunch.parent.ROSLaunchParent(rospy.get_param("/run_id"), [self.detection_1_launch_file])
+        self.detection_1_launch.start()
+
+    def start_detection_2(self):
+        rospy.loginfo("Starting detection 2")
+        self.detection_2_launch = roslaunch.parent.ROSLaunchParent(rospy.get_param("/run_id"), [self.detection_2_launch_file])
+        self.detection_2_launch.start()
+
+    def stop_detection_1(self):
+        if self.detection_1_launch is not None:
+            rospy.loginfo("Stopping detection 1")
+            self.detection_1_launch.shutdown()
+            self.detection_1_launch = None
+
+    def stop_detection_2(self):
+        if self.detection_2_launch is not None:
+            rospy.loginfo("Stopping detection 2")
+            self.detection_2_launch.shutdown()
+            self.detection_2_launch = None
+
     def shooting_done_callback(self, msg):
         self.shooting_done = bool(msg.data)
         if self.shooting_done:
             self.current_mode = 'driving'
+            self.stop_detection_2()  # Stop detection 2 when shooting is done
+            self.start_detection_1()  # Restart detection 1
         rospy.loginfo("Shooting done: %s, mode: %s", self.shooting_done, self.current_mode)
 
     def lidar_trigger_callback(self, msg):
@@ -43,6 +82,8 @@ class AutonomousVehicleNode:
         if msg.data == 1:
             self.current_mode = 'shooting'
             self.shooting_mode_pub.publish(Int32(data=1))  # 트리거 발행
+            self.stop_detection_1()  # Stop detection 1 before starting detection 2
+            self.start_detection_2()
         rospy.loginfo("Detection triggered, mode: %s", self.current_mode)
 
     def main_loop(self):
